1eee5f99107882637b22062e6f66e73ac0562de7
John Gulbronson <john@benzinga.com>
2025-07-26 11:22:55 -0400

    ethical ai

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -1,6 +1,6 @@
 import 'styled-components';
-import { Theme } from '@/styles/theme';
+import theme from '@/styles/theme';
 
 declare module 'styled-components' {
-  export type DefaultTheme = Theme;
+  export type DefaultTheme = typeof theme;
 } 
